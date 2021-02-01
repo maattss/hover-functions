91f4c78214b0f93816691b9558236770e58e7703
Siri Mykland <sirim_mykland@hotmail.com>
2021-02-01 15:31:48 +0100

    sendChallengeNotification skeleton

--- functions/src/challengeTriggers.ts
+++ functions/src/challengeTriggers.ts
@@ -1,5 +1,13 @@
 import * as functions from 'firebase-functions';
 
+
+exports.sendChallengeNotification = functions.https.onRequest(async (req, res) => {
+
+  res.status(200).json({
+    status: 'Challenge Notification Sent'
+  });
+});
+
 exports.challengeValidation = functions.https.onRequest(async (req, res) => {
 
   res.status(200).json({
